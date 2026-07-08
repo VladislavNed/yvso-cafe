230126d78172bb1427fd0ddeff36cdad71d324ae
Yana Khariebova <harebova.yana@gmail.com>
2026-07-08 14:17:54 +0200

    feat: implement reservation form and schema, update booking hooks and routes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,11 +6,17 @@ const RootLayout = () => (
     <div className="p-2 flex gap-2">
       <Link to="/" className="[&.active]:font-bold">
         Home
-      </Link>{" "}
+      </Link>
+
       <Link to="/about" className="[&.active]:font-bold">
         About
       </Link>
+
+      <Link to="/reservation" className="[&.active]:font-bold">
+        Reservierung
+      </Link>
     </div>
+
     <Outlet />
     <TanStackRouterDevtools />
   </>
